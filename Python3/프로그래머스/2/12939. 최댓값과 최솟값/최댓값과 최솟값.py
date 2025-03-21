d2aa5d4272cf88722194049efac6ed0a40babdc0
heyroha <132206033+heyroha@users.noreply.github.com>
2025-03-21 16:37:38 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.06 ms, Memory: 9.47 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -0,0 +1,11 @@
+def solution(s):
+    
+    numbers = list(map(int, s.split())) #문자열을 공백 기준으로 나눈 후, 정수로 변환하여 저장
+    
+    answer = ''
+    a = min(numbers)
+    b = max(numbers)
+    answer = f"{a} {b}"
+    print(answer)
+    
+    return answer
