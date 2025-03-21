8aa8ae22c3f5db9b00756a6f040a4c5c02ebfb40
heyroha <132206033+heyroha@users.noreply.github.com>
2025-03-21 15:20:59 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ MySQL/프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+SET @hour := -1; -- 변수 선언
+
+SELECT (@hour := @hour + 1) as HOUR,
+(SELECT COUNT(*) FROM ANIMAL_OUTS WHERE HOUR(DATETIME) = @hour) as COUNT
+FROM ANIMAL_OUTS
+WHERE @hour < 23
+
+-- GROUP BY HOUR
+-- ORDER BY HOUR ASC
